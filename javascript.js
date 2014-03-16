c367b000f15dad3a95273aa1ba9b5c4713009ae9
Pau Ferrer Ocaña <crazyserver@gmail.com>
2014-03-16 10:27:46 +0100

    Textarea not being updated by the translation file

--- javascript.js
+++ javascript.js
@@ -47,7 +47,7 @@ function loadOriginalFile(content){
     var tag = str.find("tag").text();
     var original = str.find("txt").text();
     if(original != "" && original != "undefined"){
-      $('#'+tag+'.edit').attr('value',original);
+      $('#'+tag+'.edit').val('value',original);
       var group = $('#'+tag+'.edit').parents('.form-group');
       group.removeClass("has-error");
       group.removeClass("has-warning");
